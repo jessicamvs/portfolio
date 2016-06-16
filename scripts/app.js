a92f2a447dc44fffe4529f7ff35a0dd96e66dc97
JESSICA VASQUEZ-SOLTERO <JMICHELLEVS@GMAIL.COM>
2016-06-16 11:22:45 -0700

    Created prepareProjects method to handle sort and instantiate Project objects

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,3 @@
-var projects = [];
-
 function Project (obj) {
   this.title = obj.title;
   this.category = obj.category;
@@ -7,6 +5,8 @@ function Project (obj) {
   this.publishDate = obj.publishDate;
   this.about = obj.about;
 }
+
+Project.all = [];
 
 Project.prototype.toHtml = function() {
 
@@ -24,13 +24,15 @@ Project.prototype.toHtml = function() {
   return theCompiled;
 };
 
-data.sort(function(a,b) {
-  return (new Date(b.publishDate)) - (new Date(a.publishDate));
-});
+Project.prepareProjects = function(data) {
+  data.sort(function(a,b) {
+    return (new Date(b.publishDate)) - (new Date(a.publishDate));
+  });
 
-data.forEach(function(obj) {
-  projects.push(new Project(obj));
-});
+  data.forEach(function(obj) {
+    Project.all.push(new Project(obj));
+  });
+};
 
 projects.forEach(function(obj) {
   $('#projectDisplay').append(obj.toHtml());
